d7ea0f00ee0be5a286b2c1d738b3f1ca381a1b82
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-29 13:06:42 +0000

    Implemented Navbar hiding when user scrolls down.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,18 @@
 import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import useHideOnScroll from './../../util-hooks/useHideOnScroll';
+
+const SCROLL_AMOUNT_TO_HIDE = 100;
 
 export default function MainNavigation() {
+  const showNavbar = useHideOnScroll(SCROLL_AMOUNT_TO_HIDE);
+
   return (
-    <nav className={classes.navbar} data-test="navigation-navbar">
+    <nav
+      className={`${classes.navbar} ${showNavbar ? '' : classes.hide}`}
+      data-test="navigation-navbar"
+    >
       <div className={classes.content}>
         <NavLink to="/" className={classes.logo}>
           React Meetups
