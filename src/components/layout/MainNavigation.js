16dfb8a0b7664ca09de2a5a0efedfb1f97e369f8
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-29 14:18:19 +0000

    Render amount of favorites in navbar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,11 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import useHideOnScroll from './../../util-hooks/useHideOnScroll';
+import { useContext } from 'react';
+import FavoritesContext from '../../contexts/FavoritesContext';
 
-const SCROLL_AMOUNT_TO_HIDE = 100;
+const SCROLL_AMOUNT_TO_HIDE = 50;
 
 export default function MainNavigation() {
   const showNavbar = useHideOnScroll(SCROLL_AMOUNT_TO_HIDE);
+  const { favorites } = useContext(FavoritesContext);
 
   return (
     <nav
@@ -30,7 +33,8 @@ export default function MainNavigation() {
           </li>
           <li>
             <NavLink to="/favorites" className={classes.link}>
-              My Favorites <span className={classes.badge}>{0}</span>
+              My Favorites{' '}
+              <span className={classes.badge}>{favorites.length}</span>
             </NavLink>
           </li>
         </ul>
