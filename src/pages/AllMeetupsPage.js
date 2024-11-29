be9fd54754d92c587e88b0caa2f1e5aefbd2abc5
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-29 13:36:38 +0000

    Fetch meetup data in page level instead of in item level

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,16 +1,29 @@
-import MeetupItem from "../components/meetups/MeetupItem";
-import classes from "./../components/meetups/MeetupList.module.css";
-
+import { ClipLoader } from 'react-spinners';
+import MeetupItem from '../components/meetups/MeetupItem';
+import classes from './../components/meetups/MeetupList.module.css';
+import { useFetch } from './../util-hooks/useFetch';
 
 export default function AllMeetupsPage() {
+  const { data: meetups } = useFetch({
+    url: '/data.json'
+  });
+
+  console.log('data', meetups);
+
+  if (!meetups)
+    return (
+      <div className={classes.spinner}>
+        <ClipLoader color={'#77002e'} size={80} />
+      </div>
+    );
+
   return (
     <section>
       <h1>All Meetups</h1>
       <ul className={classes.list}>
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
+        {meetups.map((meetup) => (
+          <MeetupItem item={meetup} />
+        ))}
       </ul>
     </section>
   );
